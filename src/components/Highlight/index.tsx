43eaffbe16e01d0b1a814ea9235ce8aa286dbf20
Giovanne Mendonca <giovannemendonca@outlook.com>
2023-07-08 19:51:06 -0300

    creating highlight

---
+++ src/components/Highlight/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as S from './styles'
+
+type Props = {
+  title: string
+  subtitle: string
+}
+
+export function Highlight({ title, subtitle }: Props) {
+  return (
+    <S.Container>
+      <S.Title>{title}</S.Title>
+      <S.SubTitle>{subtitle}</S.SubTitle>
+    </S.Container>
+  )
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from '@components/Header'
 import * as S from './styles'
+import { Highlight } from '@components/Highlight'
 
 export default function Groups() {
   return (
     <S.container>
       <Header />
+      <Highlight
+        title='Turmas'
+        subtitle='jogue com sua turma'
+      />
     </S.container>
   )
 }
